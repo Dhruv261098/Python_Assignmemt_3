bcb9d1975281b7a42a5f93b33a4e3b9c776c308f
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-02 11:17:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# Dhruv Desai
+# C0883865
+
+#Q1
+
+my_message = "Hello, How are you?"
+print(my_message + '\n')
+
+#Q2
+
+my_new_message = "Have a great day!!"
+print(my_new_message)
+
+print("you " + my_new_message + '\n')
+
+#Q3
+
+person = "Dhruv"
+print("hello " + person + ", would you like to take dinner today?\n")
+
+#Q4
+person_2 = "Dhruv Desai"
+print("UpperCase " + person_2.upper())
+print("LowerCase " + person_2.lower())
+print("TitleCase " + person_2.title() + '\n')
+
+#Q5
+
+print('Sir Ratan Tata once said, "Business need to go beyond the interest of their companies to the communities they serve" \n')
+
+#Q6
+
+famous_person = "Sir Ratan Tata"
+message = famous_person + ' once said, "Business need to go beyond the interest of their companies to the communities they serve"'
+print(message)
+
+#Q7
+
+person_3 = "\t Dhruv Desai \n"
+
+print(person_3)
+print(person_3.lstrip())
+print(person_3.rstrip())
+print(person_3.strip())
+
+
+#Q8
+
+print(4+4)
+print(10-2)
+print(4*2)
+print(16/2)
+
+
+#Q9
+
+favourite_No = "5"
+print("\n My favourite number is " + favourite_No)
